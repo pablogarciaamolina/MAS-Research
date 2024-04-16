09555661e097294828e4f09d74c500fd6be9002a
Javier Ríos <202111258@alu.comillas.edu>
2024-04-16 22:14:12 +0200

    Added documentation and comments to the data processing part

--- src/data.py
+++ src/data.py
@@ -1,80 +1,159 @@
 import torch
-import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset, DataLoader, random_split
 import os
-
 from transformers import BertTokenizer
 
 # Audio processing
 from scipy import signal
 from scipy.io import wavfile
-import matplotlib.pyplot as plt
 
 
-def log_specgram(audio, sample_rate, window_size=40,
-                 step_size=20, eps=1e-10):
-    nperseg = int(round(window_size * sample_rate / 1e3))
-    noverlap = int(round(step_size * sample_rate / 1e3))
+def log_specgram(audio: np.array, sample_rate: int, window_size: int = 20,
+                 step_size: int = 10, eps: float = 1e-10):
+    '''Computes the log of the spectrogram of audio data.
+    This will be used to convert the audio (.wav) files into
+    spectrograms (which are much easier to work with).
+
+    Parameters
+    ----------
+    audio : np.array
+        The audio data
+    sample_rate : int
+        The sample rate of the audio data (in Hz)
+    window_size : int
+        The size of the window for the FFT (in ms)
+    step_size : int
+        The size of the step between windows (in ms)
+    eps : float
+        A small value to avoid log(0)
+
+    Returns
+    -------
+    freqs : np.array
+        The frequencies of the spectrogram
+    spectrogram : np.array
+        The log of the spectrogram
+    '''
+    nperseg: int = int(round(window_size * sample_rate / 1e3))
+    noverlap: int = int(round(step_size * sample_rate / 1e3))
     freqs, _, spec = signal.spectrogram(audio,
-                                    fs=sample_rate,
-                                    window='hann',
-                                    nperseg=nperseg,
-                                    noverlap=noverlap,
-                                    detrend=False)
+                                        fs=sample_rate,
+                                        window='hann',
+                                        nperseg=nperseg,
+                                        noverlap=noverlap,
+                                        detrend=False)
     return freqs, np.log(spec.T.astype(np.float32) + eps)
 
 
-def audio2spectrogram(filepath):
-    samplerate, test_sound  = wavfile.read(filepath,mmap=True)
+def audio2spectrogram(filepath: str):
+    '''Converts an audio file to a spectrogram.
+
+    Parameters
+    ----------
+    filepath : str
+        The path to the audio file
+
+    Returns
+    -------
+    spectrogram : np.array
+        The log of the spectrogram
+    '''
+    # Read the audio file (memory-mapped to avoid loading the whole file)
+    samplerate, test_sound = wavfile.read(filepath, mmap=True)
+    # Compute the spectrogram
     _, spectrogram = log_specgram(test_sound, samplerate)
     return spectrogram
 
 
 def get_3d_spec(Sxx_in, moments=None):
+    '''Converts a spectrogram to a 3D tensor, with dimensions
+    (height, width, channels). The channels are the base spectrogram,
+    the first derivative, and the second derivative.
+
+    Parameters
+    ----------
+    Sxx_in : np.array
+        The input spectrogram
+    moments : tuple
+        The mean and standard deviation of the base spectrogram, the first
+        derivative, and the second derivative. If None, the mean is 0 and
+        the standard deviation is 1.
+
+    Returns
+    -------
+    np.array
+        The 3D tensor
+    '''
+    # Compute the first and second derivatives
     if moments is not None:
         (base_mean, base_std, delta_mean, delta_std,
-             delta2_mean, delta2_std) = moments
+            delta2_mean, delta2_std) = moments
+    # If moments is not provided, assume mean=0 and std=1
     else:
         base_mean, delta_mean, delta2_mean = (0, 0, 0)
         base_std, delta_std, delta2_std = (1, 1, 1)
     h, w = Sxx_in.shape
-    right1 = np.concatenate([Sxx_in[:, 0].reshape((h, -1)), Sxx_in], axis=1)[:, :-1]
+
+    right1 = np.concatenate([Sxx_in[:, 0].reshape((h, -1)),
+                             Sxx_in], axis=1)[:, :-1]
     delta = (Sxx_in - right1)[:, 1:]
     delta_pad = delta[:, 0].reshape((h, -1))
     delta = np.concatenate([delta_pad, delta], axis=1)
-    right2 = np.concatenate([delta[:, 0].reshape((h, -1)), delta], axis=1)[:, :-1]
+    right2 = np.concatenate([delta[:, 0].reshape((h, -1)),
+                             delta], axis=1)[:, :-1]
     delta2 = (delta - right2)[:, 1:]
     delta2_pad = delta2[:, 0].reshape((h, -1))
     delta2 = np.concatenate([delta2_pad, delta2], axis=1)
     base = (Sxx_in - base_mean) / base_std
     delta = (delta - delta_mean) / delta_std
     delta2 = (delta2 - delta2_mean) / delta2_std
-    stacked = [arr.reshape((h, w, 1)) for arr in (base, delta, delta2)]
+    stacked: list = [arr.reshape((h, w, 1)) for
+                     arr in (base, delta, delta2)]
     return np.concatenate(stacked, axis=2)
 
 
 class IEMOCAP_Dataset(Dataset):
+    '''Customized dataset for the IEMOCAP dataset.
+    It will contain audio, text, and emotion labels.
+    Additionally, it will apply preprocessing to both
+    audio (by converting it to a spectrogram) and text
+    (by tokenizing it using the BERT tokenizer).
+
+    Parameters
+    ----------
+    data_path : str
+        The path to the data folder (as such, the data,
+        with the audio, text, and emotion files, should
+        already be stored there).
+    '''
+
     def __init__(self, data_path: str):
+        # Paths to the folders
         self.audio_dir = os.path.join(data_path, "Audio")
         self.text_dir = os.path.join(data_path, "Text")
         self.emotion_dir = os.path.join(data_path, "Emotion/Utterances")
 
+        # Get the list of files
         self.files = os.listdir(self.audio_dir)
+        # Take only the file name (without the extension)
         self.files = [file.split(".")[0] for file in self.files]
+        # Method to tokenize text
         self.tokenizer = BertTokenizer.from_pretrained('bert-base-cased') 
 
     def __len__(self):
         return len(self.files)
 
     def __getitem__(self, idx):
+        # Get the file name
         file = self.files[idx]
 
         # Text
         text_file = os.path.join(self.text_dir, file + ".txt")
         with open(text_file, "r") as f:
             text = f.read()
-        
+
+        # Tokenize the text
         encoding = self.tokenizer.encode_plus(
                 text,
                 add_special_tokens=True,
@@ -84,23 +163,26 @@ class IEMOCAP_Dataset(Dataset):
                 pad_to_max_length=True,
                 return_attention_mask=True,
                 return_tensors='pt')
-        
-        input_ids = encoding['input_ids'].squeeze(0)
-        attention_mask = encoding['attention_mask'].squeeze(0)
-        
+
+        # Get the input_ids and attention_mask
+        input_ids: torch.Tensor = encoding['input_ids'].squeeze(0)
+        attention_mask: torch.Tensor = encoding['attention_mask'].squeeze(0)
 
         # Audio
         audio_file = os.path.join(self.audio_dir, file + ".wav")
+        # Transform the audio file to a spectrogram
         spectrogram = audio2spectrogram(audio_file)
         spectrogram = get_3d_spec(spectrogram)
         # Transpose to match PyTorch's format
         npimg = np.transpose(spectrogram, (2, 0, 1))
+        # Convert to tensor
         audio: torch.Tensor = torch.tensor(npimg)
 
         # Emotion
         emotion_file = os.path.join(self.emotion_dir, file + ".txt")
         with open(emotion_file, "r") as f:
             emotion = f.read()
+        # Convert the emotion to an integer (0-9)
         emotion = int(emotion)
 
         return audio, input_ids, attention_mask, torch.tensor(emotion)
@@ -108,12 +190,14 @@ class IEMOCAP_Dataset(Dataset):
 
 if __name__ == "__main__":
     path = "data/"
+    # Create the dataset
     dataset = IEMOCAP_Dataset(path)
 
-    # Split the dataset
+    # Split the dataset into training and testing
     train_size = int(0.8 * len(dataset))
     test_size = len(dataset) - train_size
     train_dataset, test_dataset = random_split(dataset, [train_size, test_size])
 
+    # Create the dataloaders
     train_loader = DataLoader(train_dataset, batch_size=1, shuffle=False)
     test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
--- src/file_management.py
+++ src/file_management.py
@@ -1,6 +1,5 @@
 import os
 import shutil
-import random
 
 
 def emotion_management_1():
@@ -9,15 +8,17 @@ def emotion_management_1():
     utterances are rated by multiple annotators,
     we only store the first rating.
     '''
+    # The directory containing the emotions' files
     emot_dir = "data/Not_Sorted_Emotion/Emotion"
+    # The directory where the individual .txt files will be stored
     destination_dir = "data/Emotion"
 
     # Create destination directory
     if not os.path.exists(destination_dir):
         os.makedirs(destination_dir)
 
+    # Dictionary to take only one instance of each dialog
     scenes = {}
-
     for file in os.listdir(emot_dir):
         # Check if text file
         if file.endswith(".txt"):
@@ -27,14 +28,15 @@ def emotion_management_1():
             if name_scene not in scenes:
                 # Move file to destination directory
                 source_path = os.path.join(emot_dir, file)
-                destination_path = os.path.join(destination_dir, name_scene + ".txt")
+                destination_path = os.path.join(destination_dir,
+                                                name_scene + ".txt")
                 shutil.move(source_path, destination_path)
 
                 scenes[name_scene] = True
 
 
 def emotion_management_2():
-    '''After emotion_management_1, the categorization of the emotions 
+    '''After emotion_management_1, the categorization of the emotions
     is stored in text files, corresponding one to each dialog, out of the
     28 available. This function reads the text files and stores the
     utterances in separate files, each containing the emotion of the
@@ -74,7 +76,8 @@ def emotion_management_2():
                     emotion = emotion_dict[emotion]
 
                 # Store the utterance and emotion
-                out_file = os.path.join("data/Emotion/Utterances", utterance + ".txt")
+                out_file = os.path.join("data/Emotion/Utterances",
+                                        utterance + ".txt")
                 with open(out_file, "w") as out:
                     out.write(str(emotion))
 
@@ -113,106 +116,63 @@ def text_management():
     '''With text, a similar problem arises as with emotions. Initially,
     the text is stored in a single file for each dialog. This function
     reads the text files and stores the utterances in separate files,
-    each containing the transcript of the utterance. Therefore, each 
+    each containing the transcript of the utterance. Therefore, each
     file (with the id of the utterance) contains the transcript of the
     utterance.
     '''
+    # Paths for the source and destination of the files
     text_dir = "data/Not_Sorted_Text"
     destination_dir = "data/Text"
 
+    # Take all text documents
     list_texts = os.listdir(text_dir)
 
     if not os.path.exists(destination_dir):
         os.makedirs(destination_dir)
 
+    # Iterate over the different dialogs
     for dialog in list_texts:
         with open(os.path.join(text_dir, dialog), "r") as f:
+            # Extract individual utterances
             for line in f.readlines():
                 utterance, transcript = line.split(":", 2)
 
-                # Utterance is of the format "Ses01F_impro01_F000 [1.2900-3.2100]", so we need to extract the first part
+                # Utterance is of the format "Ses01F_impro01_F000
+                # [1.2900-3.2100]", so we need to extract only the title
                 utterance = utterance.split(" ")[0]
 
                 # Store the utterance and transcript
                 out_file = os.path.join(destination_dir, utterance + ".txt")
 
-                # print(out_file)
-                if out_file == "data/Text\M.txt" or out_file == "data/Text\F.txt":
+                # Some problems arose given that some utterance isn't correctly
+                # formatted (in the format <utterance> <time> <dialog>)
+                if out_file == "data/Text\M.txt" or out_file == \
+                        "data/Text\F.txt":
                     continue
 
+                # Write the phrase
                 with open(out_file, "w") as out:
                     out.write(transcript.strip())
 
 
-def divide_data(train_prop: float = 0.8):
-    '''Divides the data into training and testing sets. The training
-    proportion is given as an argument.
-
-    Args:
-        train_prop (float, optional): The proportion of the data that
-            will be used for training. Defaults to 0.8.
-    '''
-    audio_dir = "data/Audio"
-    text_dir = "data/Text"
-    emotion_dir = "data/Emotion/Utterances"
-
-    audio_files = os.listdir(audio_dir)
-    # text_files = os.listdir(text_dir)
-    # emotion_files = os.listdir(emotion_dir)
-
-    # Create the directories
-    train_dir = "data/Training"
-    test_dir = "data/Testing"
-    os.makedirs(train_dir, exist_ok=True)
-    os.makedirs(test_dir, exist_ok=True)
-
-    for mode in ["Training", "Testing"]:
-        for modality in ["Audio", "Text", "Emotion"]:
-            os.makedirs(f"data/{mode}/{modality}", exist_ok=True)	
-
-    # Choose the training utterances
-    file_mapping = {}
-    for file in audio_files:
-        utterance = file.split(".")[0]
-        file_mapping[utterance] = {
-            "Audio": file,
-            "Text": utterance + ".txt",
-            "Emotion": utterance + ".txt"
-        }
-
-    # Shuffle the utterances
-    utterance_ids = list(file_mapping.keys())
-    print(utterance_ids[:10])
-    random.shuffle(utterance_ids)
-
-    # Number of training utterances
-    num_train = int(train_prop * len(utterance_ids))
-
-    # Move the utterances to the corresponding directories
-    for i, utterance in enumerate(utterance_ids):
-        source_dir = train_dir if i < num_train else test_dir
-        for modality in ["Audio", "Text", "Emotion"]:
-            print("file_mapping: ", file_mapping[utterance][modality])
-            source_path = os.path.join(source_dir, modality, file_mapping[utterance][modality])
-            print("Source dir: ", source_dir)
-            print("Modality: ", modality)
-            print("Utterance: ", utterance)
-            input("Press Enter to continue...")
-            destination_path = os.path.join(source_dir, modality)
-            shutil.move(source_path, destination_path)
-
-
 if __name__ == "__main__":
-    # Delete the old files
-    # if os.path.exists("data/Emotion/Utterances"):
-    #     shutil.rmtree("data/Emotion/Utterances")
-    # emotion_management_1()
-    # emotions = emotion_management_2()
-    # print(emotions)
-    # audio_management()
+    # Delete the old directories
+    if os.path.exists("data/Emotion/Utterances"):
+        shutil.rmtree("data/Emotion/Utterances")
+    if os.path.exists("data/Text"):
+        shutil.rmtree("data/Text")
+    if os.path.exists("data/Audio"):
+        shutil.rmtree("data/Audio")
 
-    # if os.path.exists("data/Text"):
-    #     shutil.rmtree("data/Text")
-    # text_management()
+    # Filter emotion texts and store the categories' dictionary
+    emotion_management_1()
+    emotions = emotion_management_2()
+    with open("data/emotions.txt", "w") as f:
+        for emotion, value in emotions.items():
+            f.write(f"{emotion}: {value}\n")
 
-    divide_data()
+    # Move the audio files
+    audio_management()
+
+    # Individualise utterances' transcriptions
+    text_management()
