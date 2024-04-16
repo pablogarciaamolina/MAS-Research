d70e0e084d631aa2502329cb3360e338814bfecd
Lucía Prado Fernández-Vega <202112679@alu.comillas.edu>
2024-04-16 20:24:28 +0200

    BERT inputs

--- models/text/pretrainedBERT.py
+++ models/text/pretrainedBERT.py
@@ -13,7 +13,7 @@ In this section, we will use the pretrained BERT model for fine-tuning. Two poss
 # Path: models/pretrainedBERT.py
 import torch
 import torch.nn as nn
-from transformers import BertModel, BertTokenizer, BertForSequenceClassification
+from transformers import BertForSequenceClassification
 
 class PretrainedBERT(nn.Module):
     """
@@ -31,11 +31,8 @@ class PretrainedBERT(nn.Module):
             1. BERT model.
             2. Classifier layer.
         """
-        self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-        #self.bert = BertModel.from_pretrained('bert-base-uncased')
         self.bert = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels = num_labels, output_attentions = True, output_hidden_states = False)
         #self.dropout = nn.Dropout(0.1)
-        self.classifier = nn.Linear(hidden_size, num_labels)
         
     def forward(self, input_ids, attention_mask):
         """
@@ -48,8 +45,7 @@ class PretrainedBERT(nn.Module):
         outputs = self.bert(input_ids, attention_mask)
         pooled_output = outputs[1]  
         #pooled_output = self.dropout(pooled_output)
-        logits = self.classifier(pooled_output)
-        return logits
+        return pooled_output
 
     def init_weights(self):
         """
@@ -58,9 +54,5 @@ class PretrainedBERT(nn.Module):
         nn.init.xavier_uniform_(self.classifier.weight)
         nn.init.constant_(self.classifier.bias, 0)
         
-if __name__ == "__main__":
-    # Test the PretrainedBERT model
-    model = PretrainedBERT(hidden_size=768, num_labels=2)
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)   
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+ 
 
