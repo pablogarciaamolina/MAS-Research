181bbadec938abbe93a2bcf6d6e9a0771514a042
Lucía Prado Fernández-Vega <202112679@alu.comillas.edu>
2024-04-18 16:56:29 +0200

    BERT

--- models/text/pretrainedBERT.py
+++ models/text/pretrainedBERT.py
@@ -1,7 +1,7 @@
 # Path: models/pretrainedBERT.py
 import torch
 import torch.nn as nn
-from transformers import BertModel
+from transformers import BertForSequenceClassification
 
 class PretrainedBERT(nn.Module):
     """
@@ -10,12 +10,12 @@ class PretrainedBERT(nn.Module):
     Args:
         num_classes: int, the number of classes.
     """
-    def __init__(self, num_labels):
+    def __init__(self):
         super(PretrainedBERT, self).__init__()
         """
         Constructor of the PretrainedBERT class.
         """
-        self.bert = BertModel.from_pretrained('bert-base-uncased', num_labels = num_labels, output_attentions = True, output_hidden_states = False)
+        self.bert = BertForSequenceClassification.from_pretrained('bert-base-uncased', output_attentions = True, output_hidden_states = False)
         self.hidden_size = self.bert.config.hidden_size
         
     def forward(self, input_ids, attention_mask):
